5e3564e80ecd3855829e498d112e5bc45c0f8c53
Rotem Kaplan <RotemKaplan@Rotem-Kaplans-Mackbook.local>
2024-11-22 13:13:52 +0200

    check

--- Algebra.java
+++ Algebra.java
@@ -6,62 +6,207 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
+	    System.out.println(plus(-5,-2));   // 2 + 3
+	    System.out.println(minus(7,-2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
+ 		System.out.println(times(-4,-4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
+   		System.out.println(pow(0,1));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
    		System.out.println(div(12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
+   		System.out.println(mod(-25,-5));   // 25 % 7
+   		System.out.println(mod(-11,3));  // 120 % 6    
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
+		System.out.println(-11%3);
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i =1;
+		int j =-1;
+		int newNumber = x1;
+		if(x2<0)
+		{
+			while(j>=x2)
+			{
+				newNumber++;
+				j--;
+			}
+			
+		}
+		if(x2>0)
+		{
+			while(i<=x2)
+			{
+				newNumber++;
+				i++;
+			}
+			
 	}
-
+	return newNumber;
+	}
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i =1;
+		int j =-1;
+		int newNumber = x1;
+		if(x2<0)
+		{
+			while(j>=x2)
+			{
+				newNumber++;;
+				j--;
+			}
+		
+		}
+		if(x2>0)
+		{
+			while(i<=x2)
+			{
+				newNumber--;;
+				i++;
+			}
+		
+		}
+		return newNumber;
+	 
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int newNumber = x1;
+		if(x2>0)
+		{
+			for(int i =1; i<x2 ; i++)
+			{
+				x1 = plus(x1, newNumber);
+			}
+		}
+		if(x2<0)
+		{
+			for(int i =-1 ; i>x2; i--)
+			{
+				x1 = plus(x1, newNumber);
+			}
+			x1 = -x1;
+		}
+		return x1;
+		
+		
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int newNumber = 1;
+		if(x<0)
+		{
+			x= -x;
+			for(int i =0; i<n ; i++)
+			{
+				newNumber =times(newNumber, x);
+			}
+			if(mod(n ,2 )==0)
+			{
+				return newNumber;
+			}
+			else{
+				return -newNumber;
+			}
+			
+		}
+		else if(x>0)
+		{
+			for(int i =0; i<n ; i++)
+			{
+				newNumber =times(newNumber, x);
+			}
+			return newNumber;
+		}
+		else
+			return 1;
+	
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int newNumber = x2;
+		int count = 0;
+		int i =1;
+		while(newNumber<=x1)
+		{
+			newNumber = plus(newNumber ,x2);
+			count++;
+			i++;
+		}
+		if(x1<0 && x2>0)
+		{
+			return -count;
+		}
+		else if(x2> 0 && x1<0)
+		{
+			return -count;
+		}
+		else if(x1 ==0)
+		{
+			return 0;
+		}
+		else
+		{
+			return count;
+		}
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if(x1< 0 && x2>0)
+		{
+			x1 = -x1;
+		}
+		else
+		{
+			x2 = -x2;
+		}
+		int newNumber = times(div(x1 ,x2), x2);
+		int theMod = minus(x1 , newNumber);
+		return theMod;
+		
+		
+		
+		
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int i =0;
+		int newNumber = 0;
+		if(x<0)
+		{
+			return -1;
+		}
+		if(x ==0)
+		{
+			return 0;
+		}
+		while(newNumber<x)
+		{
+			newNumber = pow(i,2);
+			i++;
+		}
+		if(newNumber!=x)
+		{
+			return i-2;
+		}
+		else
+		{
+			return i-1;
+		}
+		
+
+		
+		
 	}	  	  
 }
